420ae2259e4423d21e562e445f548a88aa6bc09e
karamveer-hub <68920957+karamveer-hub@users.noreply.github.com>
2023-08-16 15:35:40 +0530

    Add files via upload

--- Donna_Itallia/index2.js
+++ Donna_Itallia/index2.js
@@ -22,6 +22,10 @@ function htmlfunction(data){
 let veg_meals=document.querySelector('.veg_meals')
 let nonveg_meals=document.querySelector('.nonveg_meals')
 let pizza_veg_7inch=document.querySelectorAll('.pizza_veg_inch7')
+let pasta_veg_calories=document.querySelectorAll('.pasta_veg_calories')
+let pasta_nonveg=document.querySelectorAll('.pasta_nonveg')
+let pasta_nonveg_calories=document.querySelectorAll('.pasta_nonveg_calories')
+let pasta_veg_name=document.querySelectorAll('.pasta_veg_name')
 let pizza_nonveg_7inch=document.querySelectorAll('.pizza_nonveg_inch7')
 let pizza_gourmet_7inch=document.querySelectorAll('.pizza_quickbites_inch7')
 
@@ -32,6 +36,18 @@ let pizza_gourmet_7inch=document.querySelectorAll('.pizza_quickbites_inch7')
         element.innerText=data[i].pizza_rate_nonveg
       })
       pizza_gourmet_7inch.forEach((element,i)=>{
+        element.innerText=data[i].pizza_rate_quickbites
+      })
+      pasta_veg_calories.forEach((element,i)=>{
+        element.innerText=data[i].pizza_rate_quickbites
+      })
+      pasta_veg_name.forEach((element,i)=>{
+        element.innerText=data[i].pizza_rate_quickbites
+      })
+      pasta_nonveg.forEach((element,i)=>{
+        element.innerText=data[i].pizza_rate_quickbites
+      })
+      pasta_nonveg_calories.forEach((element,i)=>{
         element.innerText=data[i].pizza_rate_quickbites
       })
       for (let i = 0; i < data.length; i++) {
