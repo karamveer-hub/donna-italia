cdc4521e77e106465901d753fda1936e1857178c
karamveer-hub <68920957+karamveer-hub@users.noreply.github.com>
2023-08-16 13:54:28 +0530

    Add files via upload

---
+++ Donna_Itallia/index2.js
@@ -0,0 +1,44 @@
+let url='https://docs.google.com/spreadsheets/d/1g84mYlBNj2EB5C2ULFBzi2BBgSXqDrmha4pXl6k7hYc/export?format=csv';
+
+    fetch(url).then(result => result.text()).then(function (csvtext) {
+       return csv().fromString(csvtext);
+    }).then((res)=>{
+        htmlfunction(res)
+    })
+
+
+
+// setInterval(() => {
+//     data()
+// }, 10000);
+
+
+let displayDataElement=document.querySelector('#csvData')
+
+
+
+function htmlfunction(data){
+    console.log(data)
+let veg_meals=document.querySelector('.veg_meals')
+let nonveg_meals=document.querySelector('.nonveg_meals')
+let pizza_veg_7inch=document.querySelectorAll('.pizza_veg_inch7')
+let pizza_nonveg_7inch=document.querySelectorAll('.pizza_nonveg_inch7')
+let pizza_gourmet_7inch=document.querySelectorAll('.pizza_quickbites_inch7')
+
+      pizza_veg_7inch.forEach((element,i)=>{
+        element.innerText=data[i].pizza_rate_veg
+      })
+      pizza_nonveg_7inch.forEach((element,i)=>{
+        element.innerText=data[i].pizza_rate_nonveg
+      })
+      pizza_gourmet_7inch.forEach((element,i)=>{
+        element.innerText=data[i].pizza_rate_quickbites
+      })
+      for (let i = 0; i < data.length; i++) {
+        data[i].veg_meals!=""?veg_meals.innerText=data[i].veg_meals:null
+        data[i].nonveg_meals!=""?nonveg_meals.innerText=data[i].nonveg_meals:null
+        
+      }
+    }
+    
+
