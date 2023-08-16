405bc3e5efcdbd7e79423468f6070c829445fbc6
karamveer-hub <68920957+karamveer-hub@users.noreply.github.com>
2023-08-16 13:37:12 +0530

    Add files via upload

--- Donna_Itallia/index.js
+++ Donna_Itallia/index.js
@@ -1,19 +1,72 @@
+let url='https://docs.google.com/spreadsheets/d/1CJ8vrEcFwdVlUSFrvUZvEpNvdXlJPqBpGuTLgWG1FGc/export?format=csv';
 
-
-
-fetch('https://docs.google.com/spreadsheets/d/e/1UZHIgkB9tB1x_d1S-rnTcTqAN15CLoF-nKNCzKTgWxU/pub?output=csv')
-    .then(response => response.text())
-    .then(csvData => {
-        const rows = csvData.split('\n');
-        const dataList = document.getElementById('csvData');
-
-        rows.forEach(row => {
-            const columns = row.split(',');
-            const listItem = document.createElement('li');
-            listItem.textContent = columns.join(' | ');
-            dataList.appendChild(listItem);
-        });
+setInterval(() => {
+    fetch(url).then(result => result.text()).then(function (csvtext) {
+       return csv().fromString(csvtext);
+    }).then((res)=>{
+        htmlfunction(res)
     })
-    .catch(error => {
-        console.error('Error:', error);
+
+
+
+
+}, 10000);
+
+
+let displayDataElement
+let veg_meals
+let nonveg_meals
+let addon_cheese
+let addon_veggies
+let pizza_veg_12inch
+let pizza_nonveg_7inch
+let pizza_nonveg_12inch
+let pizza_gourmet_7inch
+let pizza_gourmet_12inch
+
+
+function htmlfunction(data){
+    console.log(data)
+    veg_meals=document.querySelector('.veg_meals');
+ nonveg_meals=document.querySelector('.nonveg_meals');
+ offer=document.querySelector('.offer');
+ addon_cheese=document.querySelector('.addon_cheese');
+ addon_veggies=document.querySelector('.addon_veggies');
+ pizza_veg_7inch=document.querySelectorAll('.pizza_veg_inch7');
+ pizza_veg_12inch=document.querySelectorAll('.pizza_veg_inch12');
+ pizza_nonveg_7inch=document.querySelectorAll('.pizza_nonveg_inch7');
+ pizza_nonveg_12inch=document.querySelectorAll('.pizza_nonveg_inch12');
+ pizza_gourmet_inch7=document.querySelectorAll('.pizza_gourmet_inch7');
+ pizza_gourmet_inch12=document.querySelectorAll('.pizza_gourmet_inch12');
+    // console.log(pizza_veg_7inch[0],pizza_veg_7inch[1],pizza_veg_7inch[2],pizza_veg_7inch[3],pizza_veg_7inch[4])
+    // let string=" ";
+
+    pizza_veg_7inch.forEach((element,i) => {
+        element.innerText=data[i].pizza_veg_7inch
     });
+    pizza_veg_12inch.forEach((element,i) => {
+        element.innerText=data[i].pizza_veg_12inch
+    });
+    pizza_nonveg_7inch.forEach((element,i) => {
+        element.innerText=data[i].pizza_nonveg_7inch
+    });
+    pizza_nonveg_12inch.forEach((element,i) => {
+        element.innerText=data[i].pizza_nonveg_12inch
+    });
+    pizza_gourmet_inch7.forEach((element,i) => {
+        element.innerText=data[i].gourmet_pizza_7inch
+    });
+    pizza_gourmet_inch12.forEach((element,i) => {
+        element.innerText=data[i].gourmet_pizza_12inch
+    });
+    for (let i = 0; i < data.length; i++) {
+        
+        data[i].veg_meals!=""?veg_meals.innerText=data[i].veg_meals:null
+        data[i].nonveg_meals!=""?nonveg_meals.innerText=data[i].nonveg_meals:null
+        data[i].choco_lava!=""?offer.innerText=data[i].choco_lava:null
+        data[i].addon_cheese!=""?addon_cheese.innerText=data[i].addon_cheese:null
+        data[i].addon_veggies!=""?addon_veggies.innerText=data[i].addon_veggies:null
+        
+    }
+
+}
